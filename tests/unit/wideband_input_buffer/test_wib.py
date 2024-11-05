f378a3c31e32f06c5eae423829314b3e9b7d9e95
Theo <theol0403@gmail.com>
2024-11-05 12:15:06 -0400

    CIP-3102 feat: add dish_id conversion unit tests

--- tests/unit/wideband_input_buffer/test_wib.py
+++ tests/unit/wideband_input_buffer/test_wib.py
@@ -8,6 +8,7 @@ from ska_tango_testing.integration import TangoEventTracer
 from ska_control_model import ObsState, ResultCode
 
 from ska_mid_cbf_fhs_vcc.wideband_input_buffer.wideband_input_buffer_device import WidebandInputBuffer
+from ska_mid_cbf_fhs_vcc.wideband_input_buffer.wideband_input_buffer_component_manager import convert_dish_id_uint16_t_to_mnemonic
 
 EVENT_TIMEOUT = 30
 
@@ -18,16 +19,18 @@ def pv_device():
     Fixture to set up the packet validation device for testing with a mock Tango database.
     """
     harness = context.ThreadedTestTangoContextManager()
-    harness.add_device(device_name="test/wib/1", 
-                       device_class=WidebandInputBuffer, 
-                       device_id="1",
-                       device_version_num="1.0",
-                       device_gitlab_hash="abc123",
-                       config_location="../../resources/",
-                       simulation_mode="1",
-                       emulation_mode="0",
-                       emulator_ip_block_id="wideband_input_buffer",
-                       emulator_id="vcc-emulator-1")
+    harness.add_device(
+        device_name="test/wib/1",
+        device_class=WidebandInputBuffer,
+        device_id="1",
+        device_version_num="1.0",
+        device_gitlab_hash="abc123",
+        config_location="../../resources/",
+        simulation_mode="1",
+        emulation_mode="0",
+        emulator_ip_block_id="wideband_input_buffer",
+        emulator_id="vcc-emulator-1",
+    )
 
     with harness as test_context:
         yield test_context
@@ -97,16 +100,36 @@ def test_recover_command(device_under_test):
 
     # Assertions
     assert result_code == ResultCode.OK.value, f"Expected ResultCode.OK ({ResultCode.OK.value}), got {result_code}"
-    
+
+
 def test_go_to_idle(device_under_test):
     test_configure_command(device_under_test)
-    
+
     assert device_under_test.read_attribute("obsState").value is ObsState.READY.value
-    
+
     result = device_under_test.command_inout("GoToIdle")
     result_code = result[0][0]
-    
+
     assert result_code == ResultCode.OK.value, f"Expected ResultCode.OK ({ResultCode.OK.value}), got {result_code}"
-    
+
     assert device_under_test.read_attribute("obsState").value is ObsState.IDLE.value
 
+
+def test_convert_dish_id():
+    assert convert_dish_id_uint16_t_to_mnemonic(0x0000) == "MKT000"
+    assert convert_dish_id_uint16_t_to_mnemonic(0x0063) == "MKT063"
+    assert convert_dish_id_uint16_t_to_mnemonic(0x1001) == "SKA001"
+    assert convert_dish_id_uint16_t_to_mnemonic(0x1133) == "SKA133"
+    assert convert_dish_id_uint16_t_to_mnemonic(0xFFFF) == "DIDINV"
+
+    with pytest.raises(Exception):
+        convert_dish_id_uint16_t_to_mnemonic(0x2000)  # 2 is not a dish type
+
+    with pytest.raises(Exception):
+        convert_dish_id_uint16_t_to_mnemonic(0x1000)  # SKA000 is not valid
+
+    with pytest.raises(Exception):
+        convert_dish_id_uint16_t_to_mnemonic(0x1134)  # SKA134 is not valid
+
+    with pytest.raises(Exception):
+        convert_dish_id_uint16_t_to_mnemonic(0x0064)  # MKT134 is not valid
