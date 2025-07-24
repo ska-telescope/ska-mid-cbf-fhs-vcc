8b1c6f9acf729b5b7b632f22ebb56c812253a7dd
Jason Turner <jason.turner@mda.space>
2025-07-24 16:42:01 -0300

    CIP-3707 added test logs

--- src/ska_mid_cbf_fhs_vcc/wideband_input_buffer/wideband_input_buffer_component_manager.py
+++ src/ska_mid_cbf_fhs_vcc/wideband_input_buffer/wideband_input_buffer_component_manager.py
@@ -124,7 +124,6 @@ class WidebandInputBufferComponentManager(FhsLowLevelComponentManagerBase):
     def check_registers(self: WidebandInputBufferComponentManager, status_dict: dict) -> dict[str, HealthState]:
         status: WidebandInputBufferStatus = WidebandInputBufferStatus.schema().load(status_dict)
 
-
         self.logger.info(":::::::::::::: WIB STATUES FROM EMULATOR :::::::::::::::::")
         self.logger.info(status)
 
@@ -155,7 +154,6 @@ class WidebandInputBufferComponentManager(FhsLowLevelComponentManagerBase):
                 )  # if not overflow or underflow, goes to degraded because one of packet_drop and packet_error set
         else:
             register_statuses["error"] = HealthState.OK
-
 
         self.logger.info(f":::::::: TEST_LINK_FAILURE:::::: {status.link_failure}")
 
