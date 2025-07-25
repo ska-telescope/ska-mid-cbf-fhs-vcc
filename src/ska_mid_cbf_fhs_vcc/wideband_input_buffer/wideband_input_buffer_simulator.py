b2207eb27981f67b2a7a4204dddb599c1d57197e
Jason Turner <jason.turner@mda.space>
2025-07-25 11:05:26 -0300

    CIP-3808 merged in cip-3707 current changes to work with new bitstream

--- src/ska_mid_cbf_fhs_vcc/wideband_input_buffer/wideband_input_buffer_simulator.py
+++ src/ska_mid_cbf_fhs_vcc/wideband_input_buffer/wideband_input_buffer_simulator.py
@@ -9,8 +9,8 @@ from ska_mid_cbf_fhs_common import BaseSimulatorApi
 __all__ = ["WidebandInputBufferSimulator"]
 
 
-# buffer_underflowed: bool
-# buffer_overflowed: bool
+# link_failure: bool
+# buffer_overflow: bool
 # loss_of_signal: np.uint32
 # error: bool
 # loss_of_signal_seconds: np.uint32
@@ -23,15 +23,21 @@ __all__ = ["WidebandInputBufferSimulator"]
 class WidebandInputBufferSimulator(BaseSimulatorApi):
     def __init__(self: BaseSimulatorApi, device_id: str, logger: Logger) -> None:
         self.status_str = """{
-                "buffer_underflowed": false,
-                "buffer_overflowed": false,
+                "link_failure": false,
+                "buffer_overflow": false,
                 "loss_of_signal": 0,
                 "error": false,
                 "loss_of_signal_seconds": 0,
                 "meta_band_id": 1,
                 "meta_dish_id": 1,
-                "rx_sample_rate": 3960000000,
-                "meta_transport_sample_rate": 3960000000
+                "rx_sample_rate": 3900000000,
+                "meta_transport_sample_rate": 3900000000,
+                "packet_error": false,
+                "packet_error_count": 0,
+                "packet_drop": false,
+                "packet_drop_count": 0,
+                "rx_packet_rate": 1500000,
+                "expected_sample_rate": 3900000000
             }"""
 
         super().__init__(device_id, logger)
