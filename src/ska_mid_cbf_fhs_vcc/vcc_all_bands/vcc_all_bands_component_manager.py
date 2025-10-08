363ae2b3601139e032b0fe0ad35912e6816daf4f
Jason Turner <jason.turner@mda.space>
2025-10-08 15:56:00 -0300

    CIP-4023 fix bug where wasn't checking for abort status

--- src/ska_mid_cbf_fhs_vcc/vcc_all_bands/vcc_all_bands_component_manager.py
+++ src/ska_mid_cbf_fhs_vcc/vcc_all_bands/vcc_all_bands_component_manager.py
@@ -185,13 +185,24 @@ class VCCAllBandsComponentManager(FhsControllerComponentManagerBase):
 
     def _test_host_communication(
         self,
+        task_abort_event: Event,
         task_callback: Optional[Callable] = None,
     ) -> None:
         pyro_client = PyroClient(self.logger)
 
+        task_callback(status=TaskStatus.IN_PROGRESS)
+
+        if self.task_abort_event_is_set("TestHostCommunication", task_callback, task_abort_event):
+                return
+
+
         self.logger.info("Checking connection to nameserver....")
         pyro_client.ping()
         self.logger.info("Ping finished....")
+
+        task_callback(status=TaskStatus.COMPLETED,
+                      result=(ResultCode.OK, "Host Communication OK")
+        )
 
     def _configure_scan_controller_impl(
         self,
