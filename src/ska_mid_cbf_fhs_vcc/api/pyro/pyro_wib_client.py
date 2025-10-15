5fd28344a6125025a2c768f91b6ff9d3a60b6a10
Jason Turner <jason.turner@mda.space>
2025-10-15 14:14:13 -0300

    cip-4023 fix

--- src/ska_mid_cbf_fhs_vcc/api/pyro/pyro_wib_client.py
+++ src/ska_mid_cbf_fhs_vcc/api/pyro/pyro_wib_client.py
@@ -1,9 +1,12 @@
+import parse
+
 from ska_mid_cbf_fhs_vcc.api.pyro.pyro_driver import PyroDriver
 
 
 class PyroWibClient(PyroDriver):
     def __init__(self, logger, driver_name):
         super().__init__(logger, driver_name)
+        self.location, self.local_location = self.get_location(driver_name)
 
     def configure(self):
         try:
@@ -31,3 +34,21 @@ class PyroWibClient(PyroDriver):
     def status(self, clear: bool = False):
         self.logger.info(f"::::: {self.driver_name} Driver Status :::::")
         self.logger.info(f"{super().status(clear)}")
+
+    def get_location(self, name: str):
+        "Decode the name to find the 'location' in the system."
+        forms = {
+            "ska-vcc-vcc": "{card}_receptor{lane}_",
+            "ska-base": "{card}_",
+        }
+        for category, form in forms.items():
+            form_rem = form + "{}_driver"
+            match = parse.search(form_rem, name, evaluate_result=False)
+            if match:
+                break
+        else:
+            raise ValueError(f"Name '{name}' doesn't match any of the known location strings: {forms}")
+        result = match.evaluate_result()
+        form_parts = result.named.values()
+        local_name = result.fixed[0]
+        return "-".join(form_parts), local_name
