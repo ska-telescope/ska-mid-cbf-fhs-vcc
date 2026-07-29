6a6987b12ecf5bf66b35da5c8d5838a4769f07b2
Jason Turner <jason.turner@mda.space>
2026-07-29 16:08:07 -0300

    cip-6277 fix

--- src/ska_mid_cbf_fhs_vcc/release.py
+++ src/ska_mid_cbf_fhs_vcc/release.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of the ska-mid-cbf-mcs project
-#
-# Distributed under the terms of the BSD 3-Clause license.
-# See LICENSE for more info.
-
 """Release information for SKA Mid.CBF fhs-vcc Python Package."""
 
 
 NAME = "ska_mid_cbf_fhs_vcc"
 VERSION = "0.3.14"
 VERSION_INFO = VERSION.split(".")
-DESCRIPTION = "Mid.CBF VCC Software."
+DESCRIPTION = "Mid.CBF FHS VCC Software."
 URL = "https://gitlab.com/ska-telescope/ska-mid-cbf/monitor-control/ska-mid-cbf-fhs-vcc"
 LICENSE = "BSD-3-Clause"  # noqa: A001
 COPYRIGHT = ""  # noqa: A001
