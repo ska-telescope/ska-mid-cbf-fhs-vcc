9dcd9d395e448ca62f6cedbc8c45768f2667388e
Jason Turner <jason.turner@mda.space>
2026-07-29 12:13:00 -0300

    cip-6277 updated version reporting

--- src/ska_mid_cbf_fhs_vcc/release.py
+++ src/ska_mid_cbf_fhs_vcc/release.py
@@ -19,7 +19,7 @@ NAME = "ska_mid_cbf_fhs_vcc"
 VERSION = get_release()
 VERSION_INFO = VERSION.split(".")
 DESCRIPTION = "Mid.CBF VCC Software."
-URL = "https://gitlab.com/ska-telescope/ska-mid-cbf/monitor-control/ska-mid-cbf-terabox-mgmt"
+URL = "https://gitlab.com/ska-telescope/ska-mid-cbf/monitor-control/ska-mid-cbf-fhs-vcc"
 LICENSE = "BSD-3-Clause"  # noqa: A001
 COPYRIGHT = ""  # noqa: A001
 
