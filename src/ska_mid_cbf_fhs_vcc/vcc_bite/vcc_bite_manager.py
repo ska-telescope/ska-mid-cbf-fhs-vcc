859b1552896e9207aeac81e40fa56da2a0c33557
jitansh <jitansh.arora@mda.space>
2026-08-04 20:00:54 +0000

    CIP-6288: Add start, stop and status

--- src/ska_mid_cbf_fhs_vcc/vcc_bite/vcc_bite_manager.py
+++ src/ska_mid_cbf_fhs_vcc/vcc_bite/vcc_bite_manager.py
@@ -78,12 +78,82 @@ class SPFRxPacketizerApiConfig(DataClassJsonMixin):
     noise_diode_rising_holdoff_b: float
 
 
-##
-# status class that will be populated by the APIs and returned to provide the status of VCC Stream Merge
-##
+@dataclass
+class VCCSourceSelectStatus(DataClassJsonMixin):
+    source_select: int
+    test_select: bool
+
+
 @dataclass
 class VCCBiteStatus(DataClassJsonMixin):
-    pass
+    band: int
+    start_time: int
+    sample_rate: int
+    speed: int
+
+
+@dataclass
+class VCCBiteToneGenStatus(DataClassJsonMixin):
+    sample_rate: int
+    frequency: int
+    magnitude: int
+    band: int
+
+
+@dataclass
+class GaussianNoiseDriverStatus(DataClassJsonMixin):
+    seed: int
+    mean: int
+    std_dev: int
+
+
+@dataclass
+class NoiseDiodeStatus(DataClassJsonMixin):
+    # TODO: Confirm if this field is needed, it exists in the driver on gitlab nrc config schema: sample_rate
+    switching_period: float
+    seed: int
+    std_dev: float
+
+
+@dataclass
+class PolarizationCouplerStatus(DataClassJsonMixin):
+    # TODO: Confirm if these fields are correct. They are different from the ones on NRC gitlab
+    # TODO: NRC gitlab has delay_enable: bool, correlation_coefficient: float
+    # TODO: Confirm if they are the same but just with differnt names
+    pol_coupling_rho: float
+    pol_y_1_sample_delay: bool
+
+
+@dataclass
+class SPFRxPacketizerStatus(DataClassJsonMixin):
+    running: bool
+    fifo_overflow_error: int
+    local_mac: int
+    remote_mac: int
+    ethertype: int
+    dish_id: int
+    hw_src_id: int
+    band: int
+    sample_rate: int
+    sample_rate_b: int
+    noise_diode_rising_holdoff: float
+    noise_diode_rising_holdoff_b: float
+
+
+##
+# status class that will be populated by the APIs and returned to provide the status of VCC Bite
+##
+@dataclass
+class VCCBiteManagerStatus(DataClassJsonMixin):
+    source_select: list[VCCSourceSelectStatus]
+    bite_control: list[VCCBiteStatus]
+    bite_tone_gen: list[VCCBiteToneGenStatus]
+    bite_noise_gen_polX: list[GaussianNoiseDriverStatus]  # pylint: disable=invalid-name
+    bite_noise_gen_polY: list[GaussianNoiseDriverStatus]  # pylint: disable=invalid-name
+    bite_noise_diode_polX: list[NoiseDiodeStatus]  # pylint: disable=invalid-name
+    bite_noise_diode_polY: list[NoiseDiodeStatus]  # pylint: disable=invalid-name
+    polarization_coupler: PolarizationCouplerStatus
+    spfrx_packetizer: SPFRxPacketizerStatus
 
 
 class VCCBiteManager:
@@ -265,7 +335,7 @@ class VCCBiteManager:
                 self.logger.error("Could not deconfigure Gaussian Noise Driver Y")
                 return result
 
-        # Noise Diode config
+        # Noise Diode
         for api in self._noise_diode_driver_x_apis:
             result = api.deconfigure(config=config)
             if result == 1:
@@ -277,15 +347,220 @@ class VCCBiteManager:
                 self.logger.error("Could not deconfigure Noise Diode Y")
                 return result
 
-        # Polarization Coupler Config
+        # Polarization Coupler
         result = self._polarization_coupler_api.deconfigure(config=config)
         if result == 1:
             self.logger.error("Could not deconfigure Polarization Coupler")
             return result
 
-        # TODO: Spfrx Packetizer Config
+        # TODO: Spfrx Packetizer
         # self._spfrx_packetizer_api.deconfigure(config=config)
 
         return result
 
-    # TODO: Add start, stop, status methods
+    def start(self) -> int:
+        """Start the VCC Bite."""
+        result = 0
+
+        # VCC Source Select
+        for api in self._vcc_source_select_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start VCC Source Select")
+                return result
+
+        # VCC Bite
+        for api in self._vcc_bite_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start VCC Bite")
+                return result
+
+        # VCC Bite Tone Gen
+        for api in self._vcc_bite_tone_gen_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start VCC Bite Tone Gen")
+                return result
+
+        # Gaussian Noise Driver
+        for api in self._gaussian_noise_driver_x_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start Gaussian Noise Driver X")
+                return result
+        for api in self._gaussian_noise_driver_y_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start Gaussian Noise Driver Y")
+                return result
+
+        # Noise Diode
+        for api in self._noise_diode_driver_x_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start Noise Diode X")
+                return result
+        for api in self._noise_diode_driver_y_apis:
+            result = api.start()
+            if result == 1:
+                self.logger.error("Could not start Noise Diode Y")
+                return result
+
+        # Polarization Coupler
+        result = self._polarization_coupler_api.start()
+        if result == 1:
+            self.logger.error("Could not start Polarization Coupler")
+            return result
+
+        # TODO: Spfrx Packetizer
+        # self._spfrx_packetizer_api.start()
+
+        return result
+
+    def stop(self, force: bool = False) -> int:
+        """Stop the VCC Bite."""
+        result = 0
+
+        # VCC Source Select
+        for api in self._vcc_source_select_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop VCC Source Select")
+                return result
+
+        # VCC Bite
+        for api in self._vcc_bite_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop VCC Bite")
+                return result
+
+        # VCC Bite Tone Gen
+        for api in self._vcc_bite_tone_gen_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop VCC Bite Tone Gen")
+                return result
+
+        # Gaussian Noise Driver
+        for api in self._gaussian_noise_driver_x_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop Gaussian Noise Driver X")
+                return result
+        for api in self._gaussian_noise_driver_y_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop Gaussian Noise Driver Y")
+                return result
+
+        # Noise Diode
+        for api in self._noise_diode_driver_x_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop Noise Diode X")
+                return result
+        for api in self._noise_diode_driver_y_apis:
+            result = api.stop()
+            if result == 1:
+                self.logger.error("Could not stop Noise Diode Y")
+                return result
+
+        # Polarization Coupler
+        result = self._polarization_coupler_api.stop()
+        if result == 1:
+            self.logger.error("Could not stop Polarization Coupler")
+            return result
+
+        # TODO: Spfrx Packetizer
+        # self._spfrx_packetizer_api.stop()
+
+        return result
+
+    def status(self, clear: bool = False) -> VCCBiteManagerStatus | None:
+        """Get status for all the VCC Bite components."""
+        # VCC Source Select
+        vcc_source_select_statuses = []
+        for api in self._vcc_source_select_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from VCC Source Select")
+                return status
+            vcc_source_select_statuses.append(VCCSourceSelectStatus.from_dict(status))
+
+        # VCC Bite
+        vcc_bite_statues = []
+        for api in self._vcc_bite_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from VCC Bite")
+                return status
+            vcc_bite_statues.append(VCCBiteStatus.from_dict(status))
+
+        # VCC Bite Tone Gen
+        vcc_bite_tone_gen_statuses = []
+        for api in self._vcc_bite_tone_gen_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from VCC Bite Tone Gen")
+                return status
+            vcc_bite_tone_gen_statuses.append(VCCBiteToneGenStatus.from_dict(status))
+
+        # Gaussian Noise Driver
+        gaussian_noise_driver_x_statuses = []
+        for api in self._gaussian_noise_driver_x_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from Gaussian Noise Driver X")
+                return status
+            gaussian_noise_driver_x_statuses.append(GaussianNoiseDriverStatus.from_dict(status))
+        gaussian_noise_driver_y_statuses = []
+        for api in self._gaussian_noise_driver_y_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from Gaussian Noise Driver Y")
+                return status
+            gaussian_noise_driver_y_statuses.append(GaussianNoiseDriverStatus.from_dict(status))
+
+        # Noise Diode
+        noise_diode_driver_x_statuses = []
+        for api in self._noise_diode_driver_x_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from Noise Diode X")
+                return status
+            noise_diode_driver_x_statuses.append(NoiseDiodeStatus.from_dict(status))
+        noise_diode_driver_y_statuses = []
+        for api in self._noise_diode_driver_y_apis:
+            status = api.status(clear=clear)
+            if status is None:
+                self.logger.error("Could not get status from Noise Diode Y")
+                return status
+            noise_diode_driver_y_statuses.append(NoiseDiodeStatus.from_dict(status))
+
+        # Polarization Coupler
+        polarization_coupler_status = self._polarization_coupler_api.status(clear=clear)
+        if polarization_coupler_status is None:
+            self.logger.error("Could not get status from Polarization Coupler")
+            return polarization_coupler_status
+        polarization_coupler_status = PolarizationCouplerStatus.from_dict(polarization_coupler_status)
+
+        # TODO: Spfrx Packetizer
+        spfrx_packetizer_status = self._spfrx_packetizer_api.status(clear=clear)
+        if spfrx_packetizer_status is None:
+            self.logger.error("Could not get status from SPFRx Packetizer")
+            return spfrx_packetizer_status
+        spfrx_packetizer_status = SPFRxPacketizerStatus.from_dict(spfrx_packetizer_status)
+
+        return VCCBiteManagerStatus(
+            source_select=vcc_source_select_statuses,
+            bite_control=vcc_bite_statues,
+            bite_tone_gen=vcc_bite_tone_gen_statuses,
+            bite_noise_gen_polX=gaussian_noise_driver_x_statuses,
+            bite_noise_gen_polY=gaussian_noise_driver_y_statuses,
+            bite_noise_diode_polX=noise_diode_driver_x_statuses,
+            bite_noise_diode_polY=noise_diode_driver_y_statuses,
+            polarization_coupler=polarization_coupler_status,
+            spfrx_packetizer=spfrx_packetizer_status,
+        )
